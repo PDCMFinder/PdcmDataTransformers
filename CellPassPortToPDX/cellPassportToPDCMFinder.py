9a68420717d5c3d4b1b1a5b5bb3e58114bbacdd5
afollette <follette.alex@gmail.com>
2021-09-06 12:04:47 +0100

    Update CellPassPortToPDx for "sharing" sheet
    This adds a basic sharing sheet to the cellPassportToPDCMFinder.py
    and also cleans up the code.
    
    Remove unneeded code. This codes can be found elsewhere and not
    written in Jupyter notebooks and therefore easier to maintain

--- CellPassPortToPDX/cellPassportToPDCMFinder.py
+++ CellPassPortToPDX/cellPassportToPDCMFinder.py
@@ -1,54 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[134]:
-
-
 import pandas as pd
 import sys
 import math
 
 
-# In[135]:
-
-
-from IPython.core.display import display, HTML
-display(HTML("<style>.container { width:100% !important; }</style>"))
-
-
-# In[172]:
-
-
-cellPassportDf = pd.read_excel("model_list_20210310.xlsx", engine="openpyxl")
-
-
-# In[173]:
-
-
-patient_template = pd.read_csv("metadata_template-patient.tsv", sep='\t').dropna(axis=0, how='all')
-sample_template = pd.read_csv("metadata_template-sample.tsv", sep='\t').dropna(axis=0, how='all')
-
-
-# In[174]:
-
-
 def align_and_append_sheet(template, other_sheet):
     alignment = template.align(other_sheet, join="outer")[1]
-    return template.append(alignment).dropna(axis=0, how='all')
-
-
-# In[175]:
-
-
-def transformCellPassport_patient(cellPassportDf):
-    patient_columns = ["patient_id", "gender", "smoking_status", "ethnicity"]
-    # No direct value changes are needed for the patient sheet
-    patient_column_name_changes = {"gender":"sex", "smoking_status":"history"}
-    cell_passport_patient_sheet = cellPassportDf.loc[:, patient_columns].rename(columns = patient_column_name_changes)
-    align_and_append_sheet(patient_template, cell_passport_patient_sheet).to_csv("HCMI_metadata-patient.tsv", sep='\t', index=False)
-
-
-# In[176]:
+    return template.append(alignment).dropna(axis=0, how='all').dropna(axis=1, how='all')
 
 
 def numToMon(num):
@@ -58,53 +18,113 @@ def numToMon(num):
         months = ["jan","feb","mar","apr","may","jun","jul","aug","sep","oct","nov","dec" ]
         month_str = months[num_int - 1]
     return month_str
-        
 
 
-# In[177]:
+def transformCellPassport_patient(cellPassportDf, patient_template):
+    patient_columns = ["patient_id", "gender", "smoking_status", "ethnicity"]
+    # No direct value changes are needed for the patient sheet
+    patient_column_name_changes = {"gender":"sex", "smoking_status":"history"}
+    cell_passport_patient_sheet = cellPassportDf.loc[:, patient_columns].rename(columns = patient_column_name_changes)
+    save_to_file = "HCMI_metadata-patient.tsv"
+    print(f'Saving {save_to_file}')
+    align_and_append_sheet(patient_template, cell_passport_patient_sheet).to_csv(save_to_file, sep='\t', index=False)
 
 
-sample_columns = ["patient_id", "sample_id", "model_id", "sampling_year", "sampling_month", "age_at_sampling", "cancer_type_detail", "tissue_status", "tissue", "sample_site", "tnm_t", "tnm_n", "tnm_m", "tumour_grade", "sample_treatment"]
-cell_passport_columns = cellPassportDf.loc[:, sample_columns] 
-cell_passport_sample_sheet = pd.DataFrame()
-
-columns_To_Drop = ["sampling_year", "sampling_month", "tnm_t", "tnm_n", "tnm_m", "sample_treatment"]
-sample_rename_mapping = { "age_at_sampling" : "age_in_years_at_collection", "cancer_type_detail":"diagnosis", "tissue_status":"tumour_type", "tissue" : "primary_site", "sample_site":"collection_site", "tumour_grade":  "grade" }
-cell_passport_sample_sheet = cell_passport_columns.drop(columns=columns_To_Drop).rename(columns=sample_rename_mapping)
-#Format collection date
-cell_passport_columns["sampling_month"] = cell_passport_columns["sampling_month"].apply(numToMon)
-
-
-# In[178]:
-
-
-cell_passport_sample_sheet['collection_date'] = cell_passport_columns["sampling_month"] + " " + cell_passport_columns["sampling_year"].astype(str)
-cell_passport_sample_sheet['collection_date'] = cell_passport_sample_sheet['collection_date'].str.replace(" nan", "")
-#Cat TNM staging system
-cell_passport_sample_sheet['stage'] =  cell_passport_columns["tnm_t"] + "," + cell_passport_columns["tnm_n"] + "," +  cell_passport_columns["tnm_m"]
-#Hard coded values
-cell_passport_sample_sheet["staging_system"] = "TNM staging system"
-cell_passport_sample_sheet["sharable"] = "yes"
+def transformCellPassport_sample(cellPassportDf, sample_template):
+    sample_columns = ["patient_id", "sample_id", "model_id", "sampling_year", "sampling_month", "age_at_sampling", "cancer_type_detail", "tissue_status", "tissue", "sample_site", "tnm_t", "tnm_n", "tnm_m", "tumour_grade", "sample_treatment"]
+    cell_passport_columns = cellPassportDf.loc[:, sample_columns]
+    columns_To_Drop = ["sampling_year", "sampling_month", "tnm_t", "tnm_n", "tnm_m", "sample_treatment"]
+    sample_rename_mapping = { "age_at_sampling" : "age_in_years_at_collection", "cancer_type_detail":"diagnosis", "tissue_status":"tumour_type", "tissue" : "primary_site", "sample_site":"collection_site", "tumour_grade":  "grade" }
+    cell_passport_sample_sheet = cell_passport_columns.drop(columns=columns_To_Drop).rename(columns=sample_rename_mapping)
+    #Format collection date
+    cell_passport_columns["sampling_month"] = cell_passport_columns["sampling_month"].apply(numToMon)
+    cell_passport_sample_sheet['collection_date'] = cell_passport_columns["sampling_month"] + " " + cell_passport_columns["sampling_year"].astype(str)
+    cell_passport_sample_sheet['collection_date'] = cell_passport_sample_sheet['collection_date'].str.replace(" nan", "")
+    #Cat TNM staging system
+    cell_passport_sample_sheet['stage'] =  cell_passport_columns["tnm_t"] + "," + cell_passport_columns["tnm_n"] + "," +  cell_passport_columns["tnm_m"]
+    #Hard coded values
+    cell_passport_sample_sheet["staging_system"] = "TNM staging system"
+    cell_passport_sample_sheet["sharable"] = "yes"
+    #Need to proccess sample_treatment as well still
+    save_to_file = "HCMI_metadata-sample.tsv"
+    print(f'Saving {save_to_file}')
+    align_and_append_sheet(sample_template,cell_passport_sample_sheet).to_csv(save_to_file, sep='\t', index=False)
 
 
-# In[179]:
+def transformCellPassport_other_models(cellPassportDf, other_models_template):
+    other_models_columns = ["model_id", "sample_id", "parent_id", "model_name", "model_type", "growth_properties",                            "model_comments", "model_relations_comment", "suppliers", "COSMIC_ID", "BROAD_ID", "RRID",                             "CCLE_ID", "pmed"]
+    cp_other_models_columns =  cellPassportDf.loc[:, other_models_columns]
+    other_models_rename_map = {"model_name":"name", "model_type":"type", "sample_id": "origin_patient_sample_id",                             "suppliers":"supplier", "pmed":"publications" }
+    cp_other_models_sheet = cp_other_models_columns.rename(columns=other_models_rename_map)#%%
+    cp_other_models_sheet['comments'] = cp_other_models_columns[["model_comments", "model_relations_comment"]]        .fillna('')        .agg(' '.join, axis=1)
+    cp_other_models_columns['prepended_cosmic_id'] = cp_other_models_columns['COSMIC_ID'].astype(str).apply(lambda x: if_str_not_na_prepend_str(x, "COSMIC"))
+    cp_other_models_columns['prepended_CCLE_id'] = cp_other_models_columns['CCLE_ID'].astype(str).apply(lambda x: if_str_not_na_prepend_str(x, "CCLE_Name:"))
+    cp_other_models_sheet['external_ids'] = cp_other_models_columns[['prepended_cosmic_id', 'BROAD_ID', 'RRID', "prepended_CCLE_id"]]        .fillna('')        .astype(str)        .agg(','.join, axis=1)
+    cp_other_models_sheet['external_ids'] = cp_other_models_sheet['external_ids'].str.replace(r'^,+', '', regex=True)
+    cp_other_models_sheet['external_ids'] = cp_other_models_sheet['external_ids'].str.replace(r',+$', '', regex=True)
+    cp_other_models_sheet.loc[cp_other_models_sheet["parent_id"].isna() ,"origin_patient_sample_id"] = ""
+    cp_other_models_sheet.loc[cp_other_models_sheet["parent_id"].isna() ,"origin_patient_sample_id"] = ""
+    columns_to_drop = ["model_comments", "model_relations_comment", "COSMIC_ID", "BROAD_ID", "RRID", "CCLE_ID"]
+    cp_cleaned_other_model_sheet = cp_other_models_sheet.drop(columns= columns_to_drop)
+    save_to_file = "CellModelPasport_other_models_metadata.tsv"
+    print(f'Saving {save_to_file}')
+    align_and_append_sheet(other_models_template, cp_cleaned_other_model_sheet).to_csv("CellModelPasport_other_models_metadata.tsv", sep='\t', index=False)
 
 
-tumour_type_filter = ["Metastasis", "Unknown", "Tumour"]
-filtered_passport = cell_passport_sample_sheet[cell_passport_sample_sheet["tumour_type"].isin(tumour_type_filter)]
-
-#Need to proccess sample_treatment as well still
-align_and_append_sheet(sample_template,filtered_passport).to_csv("HCMI_metadata-sample.tsv", sep='\t', index=False)
+def transformCellPassport_model_validation(filteredCpDf, model_validation_template):
+    pass
 
 
-# In[181]:
+def transformCellPassport_sharing(cellPassportDf, sharing_template):
+    CMP_sharing_sheet = pd.DataFrame()
+    CMP_sharing_sheet['model_id'] = cellPassportDf['model_id']
+    CMP_sharing_sheet['accessibility'] = 'academia and industry'
+    sharing_filename = "CellModelPasport_metadata-sharing.tsv"
+    print(f'Saving {sharing_filename}')
+    align_and_append_sheet(sharing_template, CMP_sharing_sheet).to_csv(sharing_filename, sep='\t', index=False)
 
 
-used_columns = ["gender", "smoking_status", "ethnicity",  "sample_id", "model_id", "sampling_year", "sampling_month", "age_at_sampling", "cancer_type_detail", "tissue_status", "tissue", "sample_site", "tnm_t", "tnm_n", "tnm_m", "tumour_grade", "sample_treatment"]
-cellPassportDf.drop(columns=used_columns).to_csv("leftovers.tsv", sep='\t', index=False)
+def if_str_not_na_prepend_str(str_to_check, str_to_prepend):
+    built_str = ''
+    str_to_check_arr = str_to_check.split(";")
+    space = " "
+    count = 0
+    for str_to_check in str_to_check_arr:
+        if str_to_check != 'nan' or str_to_check_arr != '':
+            built_str += f'{str_to_prepend}{str_to_check}{space*count}'
+    return built_str
 
 
-# In[ ]:
+def getTemplates():
+    patient_template = pd.read_csv("metadata_template-patient.tsv", sep='\t').dropna(axis=0, how='all')
+    sample_template = pd.read_csv("metadata_template-sample.tsv", sep='\t').dropna(axis=0, how='all')
+    other_models_template = pd.read_csv("cell_and_organoid_metadata_template-other_models.tsv", sep='\t').dropna(axis=0,
+                                                                                                                 how='all')
+    model_validation_template = pd.read_csv("metadata_template-model_validation.tsv", sep='\t').dropna(axis=0,
+                                                                                                       how='all')
+    sharing_template = pd.read_csv("metadata_template-sharing.tsv", sep='\t').dropna(axis=0, how='all')
+    return patient_template, sample_template, other_models_template, model_validation_template, sharing_template
+
+
+def filterMainDataFrame(cellPassportDf):
+    tumour_type_filter = ["Metastasis", "Unknown", "Tumour"]
+    filterIndex = cellPassportDf['HCMI'].isna() & cellPassportDf['tissue_status'].isin(tumour_type_filter) & \
+                  cellPassportDf['suppliers'].notna() & cellPassportDf['suppliers'].str.match("(?!Unknown:Unknown)")
+    return cellPassportDf[filterIndex].copy()
+
+
+def createCellPassportPdxFinderTemplates():
+    cellPassportDf = pd.read_csv("model_list_20210310.csv", engine="c", sep='\t').dropna(axis=0, how='all')
+    filteredCpDf = filterMainDataFrame(cellPassportDf)
+    patient_template, sample_template, other_models_template, model_validation_template, sharing_template = getTemplates()
+    transformCellPassport_patient(filteredCpDf, patient_template)
+    transformCellPassport_sample(filteredCpDf, sample_template)
+    transformCellPassport_other_models(filteredCpDf, other_models_template)
+    transformCellPassport_model_validation(filteredCpDf, model_validation_template)
+    transformCellPassport_sharing(filteredCpDf, sharing_template)
+
+createCellPassportPdxFinderTemplates()
+
 
 
 
